c9ca24063d16bace51d2c62074fc4c36cd169132
Tom Lee <contact@tomlee.com.au>
2018-07-15 20:50:19 -0400

    Modify theme

--- ckanext-atbr_theme/ckanext/atbr_theme/plugin.py
+++ ckanext-atbr_theme/ckanext/atbr_theme/plugin.py
@@ -10,4 +10,4 @@ class Atbr_ThemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'atbr_theme')
+        #toolkit.add_resource('fanstatic', 'atbr_theme')
