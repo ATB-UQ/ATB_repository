26db1cc4080e6d63982fe62fe708fbbc98a5934c
Tom Lee <contact@tomlee.com.au>
2018-08-23 02:35:38 -0400

    Add run field to resource schema

--- ckanext-atbrepo/ckanext/atbrepo/plugin.py
+++ ckanext-atbrepo/ckanext/atbrepo/plugin.py
@@ -3,6 +3,7 @@ import ckan.plugins.toolkit as toolkit
 
 
 class AtbrepoPlugin(plugins.SingletonPlugin):
+    # plugins.implements(p.IDatasetForm) #this  might be needed later?
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
@@ -11,3 +12,18 @@ class AtbrepoPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
        #toolkit.add_resource('fanstatic', 'atbrepo')
+
+    def _modify_package_schema(self, schema):
+        # Add run metadata field to the resource schema
+        schema['resources'].update({
+            'run' : [ tk.get_validator('is_positive_integer') ]
+        })
+        return schema
+
+    def show_package_schema(self):
+        # Validates run metadata field for resources before showing to user
+        schema['resources'].update({
+            'run' : [ tk.get_validator('is_positive_integer') ]
+        })
+        return schema
+
