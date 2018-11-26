5eaf47075c556bc194b3b6c059179603951e6504
Jordan Yee <jordan.yee@uqconnect.edu.au>
2018-11-27 08:39:05 +1000

    Fix update_config

--- ckanext-atbsearch/ckanext/atbsearch/plugin.py
+++ ckanext-atbsearch/ckanext/atbsearch/plugin.py
@@ -5,7 +5,7 @@ import ckan.plugins.toolkit as toolkit
 class AtbsearchPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
-    def update_config(self, config_):
+    def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'atbsearch')
