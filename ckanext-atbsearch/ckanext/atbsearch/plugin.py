1015399de3276ca5239f11ceded32d2a1e57aedd
Jordan Yee <jordan.yee@uqconnect.edu.au>
2018-11-27 08:16:01 +1000

    Test template change to home/index.html

--- ckanext-atbsearch/ckanext/atbsearch/plugin.py
+++ ckanext-atbsearch/ckanext/atbsearch/plugin.py
@@ -3,12 +3,9 @@ import ckan.plugins.toolkit as toolkit
 
 
 class AtbsearchPlugin(plugins.SingletonPlugin):
-    pass
-    # plugins.implements(plugins.IConfigurer)
-    #
-    # # IConfigurer
-    #
-    # def update_config(self, config_):
-    #     toolkit.add_template_directory(config_, 'templates')
-    #     toolkit.add_public_directory(config_, 'public')
-    #     toolkit.add_resource('fanstatic', 'atbsearch')
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'atbsearch')
