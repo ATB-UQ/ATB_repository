a2855987834adfa2335157f35f45cd86c40c3d6f
Jordan Yee <jordan.yee@uqconnect.edu.au>
2018-12-03 13:05:35 +1000

    Added resource formats to update.py

--- atbr_updater/update.py
+++ atbr_updater/update.py
@@ -213,6 +213,7 @@ def update_resources(
             resource_type = path.basename(resource_dir),
         )
         name, sep, ext = resource.partition(".")
+        new_data["format"] = ext
         run_str = name.rpartition('_')[-1]
         if run_str.isdigit():
             run = int(run_str)
