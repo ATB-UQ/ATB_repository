ad77da647a29287faa31cfd7411fb59103a13789
shelley-barfoot <shelleygbarfoot@gmail.com>
2021-12-10 14:34:11 +1000

    single option added
    added [single] option for --naming tag

--- Naming_Convention/rename_parser_script.py
+++ Naming_Convention/rename_parser_script.py
@@ -2,10 +2,10 @@ import argparse
 import sys
 import os
 
-parser = argparse.ArgumentParser(description='Script to rename files into standard for the ATB repository. Takes a folder containing one file extension and renames according to the argument specifications. This script should be run from the folder containing the files you would like to rename', epilog='This script was created by Shelley Barfoot, the last edit was 2/12/21.')
+parser = argparse.ArgumentParser(description='Script to rename files into standard for the ATB repository. Takes a folder containing one file extension and renames according to the argument specifications. This script should be run from the folder containing the files you would like to rename.', epilog='This script was created by Shelley Barfoot, the last edit was 2/12/21.')
 
 # -n option
-parser.add_argument('-n', '--naming', required=True, help='select the numbering system for your files. There are three accepted options: number = numbered sequentially i.e. 1, 2, 3 etc; pad = numbered with padding i.e. 00001, 00002, 00003; time = described with time frame i.e. 0-5ns, 5-10ns, 10-15ns etc.', choices=['number', 'pad', 'time'])
+parser.add_argument('-n', '--naming', required=True, help='select the numbering system for your files. There are three accepted options: number = numbered sequentially i.e. 1, 2, 3 etc; pad = numbered with padding i.e. 00001, 00002, 00003; time = described with time frame i.e. 0-5ns, 5-10ns, 10-15ns etc; single = a run containing only a single file to rename', choices=['number', 'pad', 'time', 'single'])
 
 args = parser.parse_args()
 print("Naming =", args.naming)
@@ -54,6 +54,8 @@ def rename_folder(searcher):
                 pattern = current_file_pad.zfill(5)
             elif searcher=='time':
                 pattern = '_' + str(time) + '-'
+            elif searcher=='single':
+                pattern = '.'
 
             if pattern in name:
                 #rename the file, print out for transparency
